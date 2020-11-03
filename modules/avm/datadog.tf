ad4de3f3c4fb62ad90cc3846b3c5caa1cab69e64
Fernando Gonçalves <fgoncalves@schubergphilis.com>
2020-11-03 09:02:07 +0100

    Update modules/avm/datadog.tf
    Co-authored-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- modules/avm/datadog.tf
+++ modules/avm/datadog.tf
@@ -1,7 +1,7 @@
 module "datadog" {
   count                 = var.datadog_integration == true ? 1 : 0
-  providers             = { aws = aws.managed_by_inception }
   source                = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.2"
+  providers             = { aws = aws.managed_by_inception }
   api_key               = var.datadog_api_key
   install_log_forwarder = var.datadog_install_log_forwarder
   tags                  = local.tags
