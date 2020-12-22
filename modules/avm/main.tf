a367edb6c00ea7bdb9b1a3799c2c6f787ec776bb
Sjors Gielen <sjors@sjorsgielen.nl>
2020-12-22 09:54:29 +0100

    Order account_policy resource atttributes according to style guide

--- modules/avm/main.tf
+++ modules/avm/main.tf
@@ -166,13 +166,13 @@ module "workspace" {
 }
 
 resource "aws_iam_account_password_policy" "default" {
+  count                          = var.create_password_policy ? 1 : 0
+  allow_users_to_change_password = var.allow_users_to_change_password
+  max_password_age               = var.max_password_age
   minimum_password_length        = var.minimum_password_length
   password_reuse_prevention      = var.password_reuse_prevention
   require_lowercase_characters   = var.require_lowercase_characters_in_passwords
   require_numbers                = var.require_numbers_in_passwords
-  require_uppercase_characters   = var.require_uppercase_characters_in_passwords
   require_symbols                = var.require_symbols_in_passwords
-  allow_users_to_change_password = var.allow_users_to_change_password
-  max_password_age               = var.max_password_age
-  count                          = var.create_password_policy ? 1 : 0
+  require_uppercase_characters   = var.require_uppercase_characters_in_passwords
 }
