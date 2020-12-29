545d307372b7425466ca087c3da5107483453b62
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-12-29 10:07:46 +0100

    Loosen provider version constraints
    This follows best practices as outlined in
    https://www.terraform.io/docs/configuration/version-constraints.html
    were a reusable module is less constrained and the calling/root module
    is more constrained in it's version requirements. This will allow more
    flexibility for module users on which versions they want to use.
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- modules/avm/versions.tf
+++ modules/avm/versions.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.16.0"
+      version = ">= 3.16.0"
     }
     datadog = {
       source  = "datadog/datadog"
-      version = "~> 2.14"
+      version = ">= 2.14"
     }
     github = {
       source  = "hashicorp/github"
-      version = "~> 3.1.0"
+      version = ">= 3.1.0"
     }
     mcaf = {
       source = "schubergphilis/mcaf"
     }
     tfe = {
       source  = "hashicorp/tfe"
-      version = "~> 0.21.0"
+      version = ">= 0.21.0"
     }
   }
   required_version = ">= 0.13"
--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.16.0"
+      version = ">= 3.16.0"
     }
     okta = {
       source  = "oktadeveloper/okta"
-      version = "~> 3.0"
+      version = ">= 3.0"
     }
   }
   required_version = ">= 0.13"
