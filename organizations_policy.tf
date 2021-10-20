ad6be94a73e23b3eda6102a376f1df7a37148423
alirizwansbp <80037565+alirizwansbp@users.noreply.github.com>
2021-10-20 17:23:48 +0500

    fixed for loop
    fmt
    
    ignore empty policy file
    
    fixed json
    
    Delete empty_policy.json
    
    merge require_use_of_imdsv2
    
    merge allowed regions

--- organizations_policy.tf
+++ organizations_policy.tf
@@ -1,51 +1,55 @@
 locals {
 
   enabled_policies_to_merge = {
-    deny_disabling_security_hub = var.aws_deny_disabling_security_hub
-    deny_leaving_org            = var.aws_deny_leaving_org
-    cloudtrail_log_stream       = true // This is not configurable and will be applied all the time.
+    allowed_regions_scp = {
+      enable = var.aws_region_restrictions != null ? true : false
+      policy = templatefile("${path.module}/files/organizations/allowed_regions_scp.json.tpl", {
+        allowed    = var.aws_region_restrictions.allowed
+        exceptions = var.aws_region_restrictions.exceptions
+      })
+    }
+    cloudtrail_log_stream = {
+      enable = true // This is not configurable and will be applied all the time.
+      policy = file("${path.module}/files/organizations/cloudtrail_log_stream.json")
+    }
+    deny_disabling_security_hub = {
+      enable = var.aws_deny_disabling_security_hub
+      policy = file("${path.module}/files/organizations/deny_disabling_security_hub.json")
+    }
+    deny_leaving_org = {
+      enable = var.aws_deny_leaving_org
+      policy = file("${path.module}/files/organizations/deny_leaving_org.json")
+    }
+    // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ExamplePolicies_EC2.html#iam-example-instance-metadata-requireIMDSv2
+    require_use_of_imdsv2 = {
+      enable = var.aws_require_imdsv2
+      policy = file("${path.module}/files/organizations/require_use_of_imdsv2.json")
+    }
   }
 
-  iam_policies_to_merge = [for src in local.enabled_policies_to_merge : jsondecode(
-    file(src.value == true ? "${path.module}/files/organizations/${src.key}.json" : "${path.module}/files/organizations/empty_policy.json")
+  iam_policies_to_merge = [for key, value in local.enabled_policies_to_merge : jsondecode(
+    value.enable == true ? value.policy : "{\"Statement\": []}"
   )]
 
   merged_iam_policy_statements = flatten([
     for policy in local.iam_policies_to_merge : policy.Statement
   ])
-  
+
   merged_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = local.merged_iam_policy_statements
   })
 }
 
-resource "aws_organizations_policy" "allowed_regions" {
-  count = var.aws_region_restrictions != null ? 1 : 0
-  name  = "LandingZone-AllowedRegions"
-  tags  = var.tags
-
-  content = templatefile("${path.module}/files/organizations/allowed_regions_scp.json.tpl", {
-    allowed    = var.aws_region_restrictions.allowed
-    exceptions = var.aws_region_restrictions.exceptions
-  })
-}
-
-resource "aws_organizations_policy_attachment" "allowed_regions" {
-  count     = var.aws_region_restrictions != null ? 1 : 0
-  policy_id = aws_organizations_policy.allowed_regions.0.id
-  target_id = data.aws_organizations_organization.default.roots.0.id
-}
-
-resource "aws_organizations_policy" "deny_policies" {
-  name        = "LandingZone-DenyPolicies"
-  description = "Conditionally merged: DenyDisableSecurityHub, DenyLeavingOrg and DenyDeletingCloudTrailLogStream"
+resource "aws_organizations_policy" "lz_root_policies" {
+  name        = "LandingZone-RootPolicies"
+  description = "LandingZone RootPolicies."
   content     = local.merged_policy
   tags        = var.tags
 }
 
-resource "aws_organizations_policy_attachment" "deny_policies" {
-  policy_id = aws_organizations_policy.deny_policies.id
+resource "aws_organizations_policy_attachment" "lz_root_policies" {
+  policy_id = aws_organizations_policy.lz_root_policies.id
   target_id = data.aws_organizations_organization.default.roots.0.id
 }
 
@@ -64,20 +68,6 @@ resource "aws_organizations_policy_attachment" "deny_root_user" {
 
   policy_id = aws_organizations_policy.deny_root_user.0.id
   target_id = each.value.id
-}
-
-// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ExamplePolicies_EC2.html#iam-example-instance-metadata-requireIMDSv2
-resource "aws_organizations_policy" "require_use_of_imdsv2" {
-  count   = var.aws_require_imdsv2 == true ? 1 : 0
-  name    = "LandingZone-RequireUseOfIMDSv2"
-  content = file("${path.module}/files/organizations/require_use_of_imdsv2.json")
-  tags    = var.tags
-}
-
-resource "aws_organizations_policy_attachment" "require_use_of_imdsv2" {
-  count     = var.aws_require_imdsv2 == true ? 1 : 0
-  policy_id = aws_organizations_policy.require_use_of_imdsv2.0.id
-  target_id = data.aws_organizations_organization.default.roots.0.id
 }
 
 // https://docs.aws.amazon.com/organizations/latest/userguide/orgs_manage_policies_supported-resources-enforcement.html
