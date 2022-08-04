a6171c2506323a62271c920e92a2f7cc068644d9
Marwin Baumann <mbaumann@schubergphilis.com>
2022-08-04 10:47:03 +0200

    update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.50.0, < 4.0.0"
+      version = ">= 4.9.0"
     }
   }
   required_version = ">= 1.0"
