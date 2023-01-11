34786aa73e568087ba12e8cf3a3ada4b9e2b7668
stimmerman <stimmerman@schubergphilis.com>
2023-01-11 15:39:39 +0100

    move exceptions to resources

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "mcaf" {
 
 #tfsec:ignore:AWS065
 resource "aws_cloudtrail" "additional_auditing_trail" {
+  #checkov:skip=CKV_AWS_252: "Ensure CloudTrail defines an SNS Topic"
+  #checkov:skip=CKV_AWS_35: "Ensure CloudTrail logs are encrypted at rest using KMS CMKs"
+  #checkov:skip=CKV2_AWS_10: "Ensure CloudTrail trails are integrated with CloudWatch Logs"
   count                      = var.additional_auditing_trail != null ? 1 : 0
   name                       = var.additional_auditing_trail.name
   enable_log_file_validation = true
