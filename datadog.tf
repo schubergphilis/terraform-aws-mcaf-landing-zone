39ccbeb4a1536c23f183a845820021a9337f7829
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2023-07-11 09:53:13 +0200

    enhancement: Adds log collection option for DD integration
    Signed-off-by: Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>

--- datadog.tf
+++ datadog.tf
@@ -3,24 +3,26 @@ module "datadog_audit" {
   count     = try(var.datadog.enable_integration, false) == true ? 1 : 0
   providers = { aws = aws.audit }
 
-  source                = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
-  api_key               = try(var.datadog.api_key, null)
-  excluded_regions      = var.datadog_excluded_regions
-  install_log_forwarder = try(var.datadog.install_log_forwarder, false)
-  site_url              = try(var.datadog.site_url, null)
-  tags                  = var.tags
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
+  api_key                 = try(var.datadog.api_key, null)
+  excluded_regions        = var.datadog_excluded_regions
+  install_log_forwarder   = var.datadog.install_log_forwarder
+  log_collection_services = var.datadog.log_collection_services
+  site_url                = try(var.datadog.site_url, null)
+  tags                    = var.tags
 }
 
 module "datadog_master" {
   #checkov:skip=CKV_AWS_124: since this is managed by terraform, we reason that this already provides feedback and a seperate SNS topic is therefore not required
   count = try(var.datadog.enable_integration, false) == true ? 1 : 0
 
-  source                = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
-  api_key               = try(var.datadog.api_key, null)
-  excluded_regions      = var.datadog_excluded_regions
-  install_log_forwarder = try(var.datadog.install_log_forwarder, false)
-  site_url              = try(var.datadog.site_url, null)
-  tags                  = var.tags
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
+  api_key                 = try(var.datadog.api_key, null)
+  excluded_regions        = var.datadog_excluded_regions
+  install_log_forwarder   = var.datadog.install_log_forwarder
+  log_collection_services = var.datadog.log_collection_services
+  site_url                = try(var.datadog.site_url, null)
+  tags                    = var.tags
 }
 
 module "datadog_logging" {
@@ -28,10 +30,11 @@ module "datadog_logging" {
   count     = try(var.datadog.enable_integration, false) == true ? 1 : 0
   providers = { aws = aws.logging }
 
-  source                = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
-  api_key               = try(var.datadog.api_key, null)
-  excluded_regions      = var.datadog_excluded_regions
-  install_log_forwarder = try(var.datadog.install_log_forwarder, false)
-  site_url              = try(var.datadog.site_url, null)
-  tags                  = var.tags
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.11"
+  api_key                 = try(var.datadog.api_key, null)
+  excluded_regions        = var.datadog_excluded_regions
+  install_log_forwarder   = var.datadog.install_log_forwarder
+  log_collection_services = var.datadog.log_collection_services
+  site_url                = try(var.datadog.site_url, null)
+  tags                    = var.tags
 }
