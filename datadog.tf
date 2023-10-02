fec473f8b057a19187a8aad77b50975b39a50eef
Marco Schreurs <mschreurs@schubergphilis.com>
2023-10-02 07:49:06 +0200

    update mcaf datadog to v0.3.12

--- datadog.tf
+++ datadog.tf
@@ -3,7 +3,7 @@ module "datadog_audit" {
   count     = try(var.datadog.enable_integration, false) == true ? 1 : 0
   providers = { aws = aws.audit }
 
-  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=check-latest-dd-yaml"
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.12"
   api_key                 = try(var.datadog.api_key, null)
   excluded_regions        = var.datadog_excluded_regions
   install_log_forwarder   = var.datadog.install_log_forwarder
@@ -16,7 +16,7 @@ module "datadog_master" {
   #checkov:skip=CKV_AWS_124: since this is managed by terraform, we reason that this already provides feedback and a seperate SNS topic is therefore not required
   count = try(var.datadog.enable_integration, false) == true ? 1 : 0
 
-  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=check-latest-dd-yaml"
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.12"
   api_key                 = try(var.datadog.api_key, null)
   excluded_regions        = var.datadog_excluded_regions
   install_log_forwarder   = var.datadog.install_log_forwarder
@@ -30,7 +30,7 @@ module "datadog_logging" {
   count     = try(var.datadog.enable_integration, false) == true ? 1 : 0
   providers = { aws = aws.logging }
 
-  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=check-latest-dd-yaml"
+  source                  = "github.com/schubergphilis/terraform-aws-mcaf-datadog?ref=v0.3.12"
   api_key                 = try(var.datadog.api_key, null)
   excluded_regions        = var.datadog_excluded_regions
   install_log_forwarder   = var.datadog.install_log_forwarder
