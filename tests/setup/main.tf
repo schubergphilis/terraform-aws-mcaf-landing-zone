f3111a386143c0e7bdd7094f484e0bd5b1aafece
Martin Lefler <mlefler@schubergphilis.com>
2025-08-14 15:26:18 +0200

    fix: deprecated data.aws_region.current.name (#241)
    * fix: deprecated data.aws_region.current.name
    
    * update tests to have a region attribute in mock data

--- tests/setup/main.tf
+++ tests/setup/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = ">= 5.54.0"
+      version               = ">= 6.7.0"
       configuration_aliases = [aws.audit, aws.logging]
     }
     datadog = {
