31eaebab4c36dc39e60a506ece45e85ceaa3507c
Marwin Baumann <mbaumann@schubergphilis.com>
2025-10-28 09:32:50 +0100

    use mcaf-account-baseline module for security baselines

--- security_baselines.tf
+++ security_baselines.tf
@@ -1,32 +1,42 @@
-locals {
-  security_baseline_input = {
-    regions                                     = toset(var.regions.allowed_regions)
-    aws_ebs_encryption_by_default               = var.aws_ebs_encryption_by_default
-    aws_ebs_snapshot_block_public_access_state  = var.aws_ebs_snapshot_block_public_access_state
-    aws_ssm_documents_public_sharing_permission = var.aws_ssm_documents_public_sharing_permission
-    aws_ec2_image_block_public_access_state     = var.aws_ec2_image_block_public_access_state
-    aws_account_password_policy                 = var.aws_account_password_policy
-  }
-}
-
 module "security_baseline_master" {
-  source = "./modules/security-baseline"
+  source  = "schubergphilis/mcaf-account-baseline/aws"
+  version = "~> 6.0"
 
-  security_baseline_input = local.security_baseline_input
+  account_password_policy                     = var.aws_account_password_policy
+  aws_ebs_encryption_by_default               = var.aws_ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state  = var.aws_ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state     = var.aws_ec2_image_block_public_access_state
+  aws_ssm_documents_public_sharing_permission = var.aws_ssm_documents_public_sharing_permission
+  extra_regions_to_baseline                   = local.all_organisation_regions
+  tags                                        = var.tags
 }
 
 module "security_baseline_audit" {
   providers = { aws = aws.audit }
 
-  source = "./modules/security-baseline"
+  source  = "schubergphilis/mcaf-account-baseline/aws"
+  version = "~> 6.0"
 
-  security_baseline_input = local.security_baseline_input
+  account_password_policy                     = var.aws_account_password_policy
+  aws_ebs_encryption_by_default               = var.aws_ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state  = var.aws_ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state     = var.aws_ec2_image_block_public_access_state
+  aws_ssm_documents_public_sharing_permission = var.aws_ssm_documents_public_sharing_permission
+  extra_regions_to_baseline                   = local.all_organisation_regions
+  tags                                        = var.tags
 }
 
 module "security_baseline_logging" {
   providers = { aws = aws.logging }
 
-  source = "./modules/security-baseline"
+  source  = "schubergphilis/mcaf-account-baseline/aws"
+  version = "~> 6.0"
 
-  security_baseline_input = local.security_baseline_input
+  account_password_policy                     = var.aws_account_password_policy
+  aws_ebs_encryption_by_default               = var.aws_ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state  = var.aws_ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state     = var.aws_ec2_image_block_public_access_state
+  aws_ssm_documents_public_sharing_permission = var.aws_ssm_documents_public_sharing_permission
+  extra_regions_to_baseline                   = local.all_organisation_regions
+  tags                                        = var.tags
 }
