6c682943f4342460ef3964199bdd3596ae948b85
Mayur Satpute <msatpute@schubergphilis.com>
2026-06-15 13:48:40 +0200

    Ref to new account basline feature branch

--- security_baselines.tf
+++ security_baselines.tf
@@ -15,59 +15,48 @@ locals {
   }
 }
 
-locals {
-  ebs_snapshot_block_public_access_config = var.aws_core_accounts_baseline_settings.ebs_snapshot_block_public_access_state == null ? { enabled = false } : {
-    enabled = true
-    state   = var.aws_core_accounts_baseline_settings.ebs_snapshot_block_public_access_state
-  }
-  ec2_image_block_public_access_config = var.aws_core_accounts_baseline_settings.ec2_image_block_public_access_state == null ? { enabled = false } : {
-    enabled = true
-    state   = var.aws_core_accounts_baseline_settings.ec2_image_block_public_access_state
-  }
-}
-
 module "security_baseline_master" {
-  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-config-objects-for-public-access"
+  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-unblocked-ebs-snapshot-check"
 
-  enable_additional_eu_regions                = false
-  extra_regions_to_baseline                   = local.all_governed_regions
-  account_password_policy                     = var.aws_core_accounts_baseline_settings.account_password_policy
-  aws_ebs_encryption_by_default               = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
-  aws_ebs_snapshot_block_public_access_config = local.ebs_snapshot_block_public_access_config
-  aws_ec2_image_block_public_access_config    = local.ec2_image_block_public_access_config
-  aws_kms_key_arns                            = local.kms_key_arns_management
-  aws_ssm_documents_public_sharing_permission = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
-  tags                                        = var.tags
+  enable_additional_eu_regions                    = false
+  extra_regions_to_baseline                       = local.all_governed_regions
+  account_password_policy                         = var.aws_core_accounts_baseline_settings.account_password_policy
+  aws_ebs_encryption_by_default                   = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state      = var.aws_core_accounts_baseline_settings.ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state         = var.aws_core_accounts_baseline_settings.ec2_image_block_public_access_state
+  aws_kms_key_arns                                = local.kms_key_arns_management
+  aws_ssm_documents_public_sharing_permission     = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
+  tags                                            = var.tags
 }
 
 module "security_baseline_audit" {
   providers = { aws = aws.audit }
 
-  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-config-objects-for-public-access"
+  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-unblocked-ebs-snapshot-check"
 
-  enable_additional_eu_regions                = false
-  extra_regions_to_baseline                   = local.all_governed_regions
-  account_password_policy                     = var.aws_core_accounts_baseline_settings.account_password_policy
-  aws_ebs_encryption_by_default               = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
-  aws_ebs_snapshot_block_public_access_config = local.ebs_snapshot_block_public_access_config
-  aws_ec2_image_block_public_access_config    = local.ec2_image_block_public_access_config
-  aws_kms_key_arns                            = local.kms_key_arns_audit
-  aws_ssm_documents_public_sharing_permission = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
-  tags                                        = var.tags
+  enable_additional_eu_regions                    = false
+  extra_regions_to_baseline                       = local.all_governed_regions
+  account_password_policy                         = var.aws_core_accounts_baseline_settings.account_password_policy
+  aws_ebs_encryption_by_default                   = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state      = var.aws_core_accounts_baseline_settings.ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state         = var.aws_core_accounts_baseline_settings.ec2_image_block_public_access_state
+  aws_kms_key_arns                                = local.kms_key_arns_audit
+  aws_ssm_documents_public_sharing_permission     = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
+  tags                                            = var.tags
 }
 
 module "security_baseline_logging" {
   providers = { aws = aws.logging }
 
-  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-config-objects-for-public-access"
+  source = "git::https://github.com/schubergphilis/terraform-aws-mcaf-account-baseline.git?ref=add-unblocked-ebs-snapshot-check"
 
-  enable_additional_eu_regions                = false
-  extra_regions_to_baseline                   = local.all_governed_regions
-  account_password_policy                     = var.aws_core_accounts_baseline_settings.account_password_policy
-  aws_ebs_encryption_by_default               = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
-  aws_ebs_snapshot_block_public_access_config = local.ebs_snapshot_block_public_access_config
-  aws_ec2_image_block_public_access_config    = local.ec2_image_block_public_access_config
-  aws_kms_key_arns                            = local.kms_key_arns_logging
-  aws_ssm_documents_public_sharing_permission = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
-  tags                                        = var.tags
+  enable_additional_eu_regions                    = false
+  extra_regions_to_baseline                       = local.all_governed_regions
+  account_password_policy                         = var.aws_core_accounts_baseline_settings.account_password_policy
+  aws_ebs_encryption_by_default                   = var.aws_core_accounts_baseline_settings.ebs_encryption_by_default
+  aws_ebs_snapshot_block_public_access_state      = var.aws_core_accounts_baseline_settings.ebs_snapshot_block_public_access_state
+  aws_ec2_image_block_public_access_state         = var.aws_core_accounts_baseline_settings.ec2_image_block_public_access_state
+  aws_kms_key_arns                                = local.kms_key_arns_logging
+  aws_ssm_documents_public_sharing_permission     = var.aws_core_accounts_baseline_settings.ssm_documents_public_sharing_permission
+  tags                                            = var.tags
 }
