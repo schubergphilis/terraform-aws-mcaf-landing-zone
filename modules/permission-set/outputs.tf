2f941b5d5bbbd04940b9e8ed6b9a0ff2daeca52a
Frank van Boven <frank@cenotaph.nl>
2026-06-25 14:41:24 +0200

    Add Permissins set ame output

--- modules/permission-set/outputs.tf
+++ modules/permission-set/outputs.tf
@@ -2,3 +2,9 @@ output "arn" {
   description = "The ARN of the permission set."
   value       = local.permission_set_arn
 }
+
+output "name" {
+  description = "The Name of the permission set."
+  value       = local.permission_set_name
+}
+
